6f79956c558d4bdad31519d9601450a354939e34
Shakya77 <bijanshakya145@gmail.com>
2025-03-07 17:14:25 +0545

    changes done in

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, TouchableOpacity, Pressable } from "react-native";
+import { styles } from "../styles/auth.styles";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style={styles.title}>
         Welcome to React Native!
       </Text>
+      <TouchableOpacity onPress={() => alert("you touched")}>
+        <Text>Click me</Text>
+      </TouchableOpacity>
+      <Pressable onPress={() => alert("you pressed")}>
+        <Text>Press me</Text>
+      </Pressable>
     </View>
   );
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title:{
-    color:'red',
-    fontSize:20,
-  }
-});
