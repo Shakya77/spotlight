ef4bbfdb56a8caf9ae663e87e1a674cf8d359202
Shakya77 <bijanshakya145@gmail.com>
2025-03-07 20:08:36 +0545

    changes done in intex.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TouchableOpacity, Pressable } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import { styles } from "../styles/auth.styles";
 
 export default function Index() {
@@ -7,12 +7,20 @@ export default function Index() {
       <Text style={styles.title}>
         Welcome to React Native!
       </Text>
-      <TouchableOpacity onPress={() => alert("you touched")}>
-        <Text>Click me</Text>
+
+      <Image source={{
+        uri:
+          "https://images.unsplash.com/photo-1741217531054-d00a1165dad5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }}
+        style={{
+          width: 100,
+          height: 100,
+          resizeMode: "cover",
+        }}></Image>
+
+      <TouchableOpacity >
+        <Text> this is the </Text>
       </TouchableOpacity>
-      <Pressable onPress={() => alert("you pressed")}>
-        <Text>Press me</Text>
-      </Pressable>
     </View>
   );
 }
