63c89158b8205c1449ee61a8f84ced668aa967bb
Shakya77 <bijanshakya145@gmail.com>
2025-03-09 23:03:45 +0545

    changes done in layout.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,11 @@
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image } from "react-native";
 import { styles } from "../styles/auth.styles";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Welcome to React Native!
-      </Text>
-
-      <Image source={{
-        uri:
-          "https://images.unsplash.com/photo-1741217531054-d00a1165dad5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      }}
-        style={{
-          width: 100,
-          height: 100,
-          resizeMode: "cover",
-        }}></Image>
-
-      <TouchableOpacity >
-        <Text> this is the </Text>
-      </TouchableOpacity>
+      <Link href={"/notifications"}>Visit Notifications Screen</Link>
     </View>
   );
 }
